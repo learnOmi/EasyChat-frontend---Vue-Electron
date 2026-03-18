a1b07b1fdf5bf24fa8bd76c2a4da55975a7dc289
Tong <1427275295@qq.com>
2026-03-19 00:14:21 +0800

    preliminary compeletion of About and FileManage component

--- src/renderer/src/views/setting/About.vue
+++ src/renderer/src/views/setting/About.vue
@@ -1,9 +1,25 @@
 <template>
-  <div>关于</div>
+  <ContentPanel>
+    <el-form ref="formDataRef" label-width="80px" @submit.prevent>
+      <el-form-item label="版本号">
+        <div class="version-info">
+          <div>EasyChat {{ config.version }}</div>
+          <div>
+            <el-button type="primary" @click="checkUpdate">检查更新</el-button>
+          </div>
+        </div>
+      </el-form-item>
+    </el-form>
+  </ContentPanel>
 </template>
 
 <script setup>
+import config from '../../../../../package.json'
+import { ref, reactive, getCurrentInstance, nextTick } from "vue"
+const { proxy } = getCurrentInstance()
 
+// TODO 检测更新
+const checkUpdate = () => {}
 </script>
 
 <style lang="scss" scoped>
--- src/renderer/src/views/setting/FileManage.vue
+++ src/renderer/src/views/setting/FileManage.vue
@@ -1,12 +1,65 @@
 <template>
-  <ContentPanel>
-  
+  <ContentPanel v-loading="copying" element-loading-text="正在复制文件，请稍后...">
+    <el-form
+      ref="formDataRef"
+      label-position="top"
+      :model="formData"
+      :rules="rules"
+      label-width="80px"
+      @submit.prevent
+    >
+      <!--input输入-->
+      <el-form-item label="文件管理" prop="" class="file-manage">
+        <div class="file-input" :title="formData.sysSetting">{{ formData.sysSetting }}</div>
+        <div class="tips">文件的默认保存位置</div>
+      </el-form-item>
+      <el-form-item label="" prop="">
+        <el-button type="primary" @click="changeFolder">更改</el-button>
+        <el-button type="primary" @click="openLocalFolder">打开文件夹</el-button>
+      </el-form-item>
+    </el-form>
   </ContentPanel>
 </template>
 
 <script setup>
+import { ref, reactive, getCurrentInstance, nextTick } from 'vue'
+const { proxy } = getCurrentInstance()
 
+const copying = ref(false)
+
+const formData = ref({})
+const formDataRef = ref()
+const rules = {
+  title: [{ required: true, message: '请输入内容' }]
+}
+
+// TODO 获取文件缓存路径
+
+const changeFolder = () => {
+  
+}
+
+const openLocalFolder = () => {
+  
+}
 </script>
 
 <style lang="scss" scoped>
+.file-manage {
+  :deep(.el-form-item__content) {
+    display: block;
+  }
+  .file-input {
+    background: #fff;
+    padding: 0px 5px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    font-size: 16px;
+  }
+  .tips {
+    color: #888888;
+    font-size: 13px;
+  }
+}
 </style>
